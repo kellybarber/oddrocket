9d2d7dff7f8092e05e41cd567c3ba23240984839
Kelly Barber <Kelly@Kellys-MacBook-Air.local>
2019-03-19 12:59:22 -0600

    # Feature: Added basic nav styling

--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="main-navigation">
+    <div class="nav-divider"></div>
     <router-link
       v-for="(link, index) in routes"
       :key="index"
       :to="link.route"
       class="nav-link"
     >
-      {{ link.title }}
+      <span class="link-index">{{ formatIndex(index) }}</span>
+      <span class="link-title btn-standard">{{ link.title }}</span>
     </router-link>
   </div>
 </template>
@@ -20,6 +22,12 @@
         { title : 'About', route : '/about' },
         { title : 'Contact', route : '/contact' }
       ];
+    },
+    methods : {
+      formatIndex(index) {
+        const paddedIndex = index < 10 ? `0${index}` : index;
+        return `.${paddedIndex}`;
+      }
     }
   }
 </script>
@@ -30,17 +38,37 @@
     position: fixed;
     top: 0;
     left: 0;
-    display: flex;
-    flex-direction: column;
     height: 100%;
-    width: 300px;
+    width: 30rem;
     padding: 20rem 5rem 0 5rem;
-    background-color: var(--white);
+    background-color: #E6ECF2;
     z-index: 3;
   }
 
+  .nav-divider {
+    position: absolute;
+    top: 15rem;
+    left: 12rem;
+    height: 60%;
+    width: 1px;
+    background-color: var(--black);
+  }
+
   .nav-link {
+    display: inline-block;
+    float: left;
+    clear: both;
+    font-size: 1.6em;
+    font-weight: 200;
+    color: var(--black);
     text-decoration: none;
+  }
+  .nav-link:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
+  .link-index {
+    margin-right: 10rem;
   }
 
 </style>
