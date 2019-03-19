89e875e8ca1e8bf7d7d8c31894cb567981917bf7
Kelly Barber <Kelly@Kellys-MacBook-Air.local>
2019-03-19 13:03:35 -0600

    # Feature: Created about section

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,34 @@
-import Home from '../sections/Home';
-import Work from '../sections/Work';
-import Film from '../sections/Film';
+import Home    from '../sections/Home';
+import Work    from '../sections/Work';
+import Film    from '../sections/Film';
+import About   from '../sections/About';
 import Contact from '../sections/Contact';
 
 export default [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path      : '/',
+    name      : 'home',
+    component : Home
   },
   {
-    path: '/work',
-    name: 'work',
-    component: Work
+    path      : '/work',
+    name      : 'work',
+    component : Work
   },
   {
-    path: '/work/film/:id',
-    name: 'film',
-    component: Film
+    path      : '/work/film/:id',
+    name      : 'film',
+    component : Film
   },
   {
-    path: '/contact',
-    name: 'contact',
-    component: Contact
+    path      : '/about',
+    name      : 'about',
+    component : About
+  },
+  {
+    path      : '/contact',
+    name      : 'contact',
+    component : Contact
   },
   {
     path     : '*',
---
+++ src/sections/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <SectionWrapper>
+
+  </SectionWrapper>
+</template>
+
+<script>
+  import SectionWrapper from '../wrappers/SectionWrapper';
+
+  export default {
+    components : { SectionWrapper }
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
